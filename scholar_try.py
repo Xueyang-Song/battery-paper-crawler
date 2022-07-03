157e3a1883da43d215b4058e6cfe6f569c670989
Melody Song <songxueyang94@gmail.com>
2022-07-02 23:18:00 -0700

    more fixes

--- scholar_try.py
+++ scholar_try.py
@@ -8,7 +8,9 @@ headers = {"User-Agent": "Mozilla/5.0"}
 response = requests.get(url, headers=headers)
 soup = BeautifulSoup(response.text, "html.parser")
 
-boxes = soup.find_all("div", {"class": "gs_r"})
+boxes = soup.find_all("div", {"class": "gs_r gs_or gs_scl"})
 print("found", len(boxes))
 for box in boxes:
-    print(box.get_text()[:80])
+    title = box.find("h3")
+    if title:
+        print(title.get_text(" ", strip=True))
