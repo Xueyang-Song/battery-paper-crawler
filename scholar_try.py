b55a6aef0595bb57dd2881c80e8456410dba03d2
Melody Song <songxueyang94@gmail.com>
2022-07-10 22:41:00 -0700

    can search with words now

--- scholar_try.py
+++ scholar_try.py
@@ -1,15 +1,16 @@
+import sys
 import requests
 from bs4 import BeautifulSoup
 
-query = "zinc ion battery machine learning"
-url = "https://scholar.google.com/scholar?q=" + query.replace(" ", "+")
+query = " ".join(sys.argv[1:])
+search_text = query + " filetype:pdf"
+url = "https://scholar.google.com/scholar?q=" + search_text.replace(" ", "+")
 headers = {"User-Agent": "Mozilla/5.0"}
 
 response = requests.get(url, headers=headers)
 soup = BeautifulSoup(response.text, "html.parser")
 
 boxes = soup.find_all("div", {"class": "gs_r gs_or gs_scl"})
-print("found", len(boxes))
 for box in boxes:
     title = box.find("h3")
     if title:
