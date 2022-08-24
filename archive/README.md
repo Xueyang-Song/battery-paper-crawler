97bcb0b096924e926481caac6b3bf335893d551a
Melody Song <songxueyang94@gmail.com>
2022-08-23 21:46:00 -0700

    saved june papers

---
+++ archive/README.md
@@ -0,0 +1,16 @@
+# Crawl Archive
+
+This folder keeps the month by month scholar crawl trail inside the same repo.
+
+What is here:
+- `monthly_runs/YYYY-MM/<query>/` keeps a normalized manifest and the search result metadata for that month
+- `paper_library/` stores each unique pdf only once, even if the same paper showed up again in later months
+
+Range right now:
+- first month: `2022-06`
+- latest month: `2022-06`
+- unique pdf files in library: `16`
+
+Note:
+- the first few 2022 months were backfilled once the crawler stopped breaking enough to save things
+- repeated monthly recrawls point back into the shared pdf library instead of copying the same file over and over
--- crawler.py
+++ crawler.py
@@ -1,11 +1,14 @@
 import os
 import sys
 import time
+from urllib.parse import quote_plus
+
 import requests
 from bs4 import BeautifulSoup
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9",
 }
 
 limit = 3
@@ -29,7 +32,7 @@ if len(query_parts) == 0:
 
 query = " ".join(query_parts)
 search_text = query + " filetype:pdf"
-folder_name = query.replace(" ", "_")
+folder_name = query.replace(" ", "_").replace("/", "_")
 
 if not os.path.exists("downloads"):
     os.mkdir("downloads")
@@ -37,47 +40,71 @@ if not os.path.exists(os.path.join("downloads", folder_name)):
     os.mkdir(os.path.join("downloads", folder_name))
 
 if htmlfile:
-    f = open(htmlfile, "r", encoding="utf-8")
-    html = f.read()
-    f.close()
+    print("using html file", htmlfile)
+    with open(htmlfile, "r", encoding="utf-8") as handle:
+        html = handle.read()
 else:
-    scholar_url = "https://scholar.google.com/scholar?q=" + search_text.replace(" ", "+")
+    scholar_url = "https://scholar.google.com/scholar?q=" + quote_plus(search_text)
+    print("getting scholar page")
+    print(scholar_url)
     response = requests.get(scholar_url, headers=headers, timeout=25)
+    print("status", response.status_code)
+    if response.status_code != 200:
+        print("scholar did not work")
+        sys.exit()
     html = response.text
+
 
 soup = BeautifulSoup(html, "html.parser")
 wraps = soup.find_all("div", {"class": "gs_r gs_or gs_scl"})
+if len(wraps) == 0:
+    wraps = soup.find_all("div", {"class": "gs_ri"})
+
+print("results found", len(wraps))
 done = 0
 
 for wrap in wraps:
     if done >= limit:
         break
     try:
-        info = wrap.find("div", {"class": "gs_ri"})
-        side = wrap.find("div", {"class": "gs_or_ggsm"})
+        if wrap.get("class") and "gs_ri" in wrap.get("class"):
+            info = wrap
+            side = None
+        else:
+            info = wrap.find("div", {"class": "gs_ri"})
+            side = wrap.find("div", {"class": "gs_or_ggsm"})
         if not info:
             continue
         h3 = info.find("h3")
         if not h3:
             continue
-        title = h3.get_text(" ", strip=True)
+        title = h3.get_text(" ", strip=True).replace("[PDF] ", "")
         print("title", title)
         a = h3.find("a")
-        link = ""
+        use_link = ""
         if side and side.find("a"):
-            link = side.find("a").get("href", "")
+            use_link = side.find("a").get("href", "")
         elif a:
-            link = a.get("href", "")
-        if link == "":
+            use_link = a.get("href", "")
+        if use_link == "":
             continue
-        pdf_r = requests.get(link, headers=headers, timeout=25)
+        pdf_r = requests.get(use_link, headers=headers, timeout=25)
+        ctype = pdf_r.headers.get("content-type", "").lower()
         if pdf_r.status_code != 200:
             continue
-        name = title[:80].replace("/", "_")
-        out = open(os.path.join("downloads", folder_name, name + ".pdf"), "wb")
-        out.write(pdf_r.content)
-        out.close()
+        if ".pdf" not in use_link.lower() and "pdf" not in ctype:
+            continue
+        name = title[:80]
+        for bad in '\\/:*?"<>|':
+            name = name.replace(bad, "_")
+        out_path = os.path.join("downloads", folder_name, name + ".pdf")
+        with open(out_path, "wb") as out:
+            out.write(pdf_r.content)
+        print("saved", out_path)
         done = done + 1
         time.sleep(2)
     except Exception as e:
-        print("one result failed", e)
+        print("one result failed")
+        print(e)
+
+print("done got", done, "pdfs")
